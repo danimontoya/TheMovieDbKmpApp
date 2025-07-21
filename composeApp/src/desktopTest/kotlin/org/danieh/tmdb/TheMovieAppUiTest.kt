13e54d4c32619751380e2f42b960e42fcb9212e7
danielmontoya <dmontoya@jackhenry.com>
2025-07-21 10:34:16 +0200

    Fix navigation ui-test LocalLifecycleOwnerFake

--- composeApp/src/desktopTest/kotlin/org/danieh/tmdb/TheMovieAppUiTest.kt
+++ composeApp/src/desktopTest/kotlin/org/danieh/tmdb/TheMovieAppUiTest.kt
@@ -4,15 +4,21 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.setMain
 import kotlinx.datetime.LocalDate
 import org.danieh.tmdb.domain.model.Genre
 import org.danieh.tmdb.domain.model.Movie
 import org.junit.Rule
 import org.junit.Test
+import kotlin.test.BeforeTest
 
 class TheMovieAppUiTest {
 
@@ -41,31 +47,57 @@ class TheMovieAppUiTest {
         )
     )
 
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @BeforeTest
+    fun setup() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
     @Test
-    fun `launch app and display movies`() {
-        composeTestRule.runOnIdle {
-            composeTestRule.setContent {
-                CompositionLocalProvider(
-                    LocalLifecycleOwner provides LocalLifecycleOwnerFake(),
-                ) {
-                    with(dIScope) {
-                        TheMovieApp(
-                            popularMoviesViewModelFactory,
-                            movieDetailsViewModelFactory
-                        )
-                    }
+    fun `display popular movies and navigate to details`() {
+        val lifecycleOwnerFake = LocalLifecycleOwnerFake()
+
+        composeTestRule.setContent {
+            CompositionLocalProvider(
+                LocalLifecycleOwner provides lifecycleOwnerFake,
+            ) {
+                with(dIScope) {
+                    TheMovieApp(
+                        popularMoviesViewModelFactory,
+                        movieDetailsViewModelFactory
+                    )
                 }
             }
+        }
+
+        composeTestRule.runOnIdle {
+            lifecycleOwnerFake.markResumed()
         }
 
         composeTestRule
             .onNodeWithText("Kraven the Hunter")
             .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Kraven the Hunter")
+            .performClick()
+
+        composeTestRule
+            .onNodeWithText("Overview")
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNodeWithText("Kraven Kravinoff's complex relationship with his ruthless gangster father, Nikolai, starts him down a path of vengeance with brutal consequences, motivating him to become not only the greatest hunter in the world, but also one of its most feared.")
+            .assertIsDisplayed()
     }
 }
 
 private class LocalLifecycleOwnerFake : LifecycleOwner {
-    override val lifecycle: Lifecycle = LifecycleRegistry(this).apply {
-        currentState = Lifecycle.State.RESUMED
+    private val registry = LifecycleRegistry(this)
+
+    override val lifecycle: Lifecycle get() = registry
+
+    fun markResumed() {
+        registry.currentState = Lifecycle.State.RESUMED
     }
 }
